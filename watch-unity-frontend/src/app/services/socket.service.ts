c343018c5d7d7793a7baeed5eb13626ddeb9676f
sivaprasadvitta <sivaprasad.vitta@.com>
2025-03-22 01:20:31 +0530

    backend url is updated

--- watch-unity-frontend/src/app/services/socket.service.ts
+++ watch-unity-frontend/src/app/services/socket.service.ts
@@ -11,7 +11,7 @@ export class SocketService {
   videoId:string | null="";
 
   constructor() {
-    this.socket = io('http://localhost:5000');
+    this.socket = io('https://watchunity-backend.onrender.com');
   }
 
   joinRoom(roomId: string) {
